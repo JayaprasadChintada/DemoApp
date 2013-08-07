2ff7971bbbbac4f82f9c96d57c876f4097344a8a
jaya <jaya@gmail.com>
2013-08-08 00:56:34 +0530

    branch 2 initial commit with non static block

--- folder1/A.java
+++ folder1/A.java
@@ -2,5 +2,11 @@ public class A{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
+	static{
+		System.out.println("Stati block will be executed first");
+	}
+	{
+		System.out.println("Non static blocks are executed at the time of instantiation ");
+	}
 	
 }
